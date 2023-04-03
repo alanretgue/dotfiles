60ea18fd272513fac102467b04f6b711a03425d7
Alan GUERET <alan.gueret@epita.fr>
2023-04-03 08:44:31 +0200

    Fix Nvim config

--- config/nvim/init.lua
+++ config/nvim/init.lua
@@ -40,30 +40,5 @@ end
 vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup("plugins")
+require("keybindings")
 
--- ###########################################
--- ############## Tab handling ###############
--- ###########################################
---
-map('n', '<space>t+', '<Cmd>tabnew<CR>', opts)
--- Move to previous/next
-map('n', '<space>tp', '<Cmd>BufferPrevious<CR>', opts)
-map('n', '<space>tn', '<Cmd>BufferNext<CR>', opts)
--- Re-order to previous/next
-map('n', '<space>tP', '<Cmd>BufferMovePrevious<CR>', opts)
-map('n', '<space>tN', '<Cmd>BufferMoveNext<CR>', opts)
--- Goto buffer in position...
-map('n', '<space>t1', '<Cmd>BufferGoto 1<CR>', opts)
-map('n', '<space>t2', '<Cmd>BufferGoto 2<CR>', opts)
-map('n', '<space>t3', '<Cmd>BufferGoto 3<CR>', opts)
-map('n', '<space>t4', '<Cmd>BufferGoto 4<CR>', opts)
-map('n', '<space>t5', '<Cmd>BufferGoto 5<CR>', opts)
-map('n', '<space>t6', '<Cmd>BufferGoto 6<CR>', opts)
-map('n', '<space>t7', '<Cmd>BufferGoto 7<CR>', opts)
-map('n', '<space>t8', '<Cmd>BufferGoto 8<CR>', opts)
-map('n', '<space>t9', '<Cmd>BufferGoto 9<CR>', opts)
-map('n', '<space>tl', '<Cmd>BufferLast<CR>', opts)
--- Pin/unpin buffer
-map('n', '<space>tm', '<Cmd>BufferPin<CR>', opts)
--- Close buffer
-map('n', '<space>tc', '<Cmd>BufferClose<CR>', opts)
--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -10,37 +10,75 @@ return {
         end,
     },
     {
-        "neovim/nvim-lspconfig",
-        lazy = false,
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v1.x',
+        dependencies = {
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},             -- Required
+            {'williamboman/mason.nvim'},           -- Optional
+            {'williamboman/mason-lspconfig.nvim'}, -- Optional
+
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},         -- Required
+            {'hrsh7th/cmp-nvim-lsp'},     -- Required
+            {'hrsh7th/cmp-buffer'},       -- Optional
+            {'hrsh7th/cmp-path'},         -- Optional
+            {'saadparwaiz1/cmp_luasnip'}, -- Optional
+            {'hrsh7th/cmp-nvim-lua'},     -- Optional
+
+            -- Snippets
+            {'L3MON4D3/LuaSnip'},             -- Required
+            {'rafamadriz/friendly-snippets'}, -- Optional
+        },
         config = function()
-            require'lspconfig'.rust_analyzer.setup{
-                settings = {
-                    ['rust-analyzer'] = {},
-                },
-            }
-            require'lspconfig'.clangd.setup{}
+            local lsp = require('lsp-zero').preset({
+                name = 'minimal',
+                set_lsp_keymaps = true,
+                manage_nvim_cmp = true,
+                suggest_lsp_servers = false,
+            })
+
+            -- (Optional) Configure lua language server for neovim
+            lsp.nvim_workspace()
+
+            lsp.setup()
         end,
     },
     {
-        "ibhagwan/fzf-lua",
-        lazy = true,
-        keys = {
-            -- { "<space>fz", "<cmd>FzfLua files<cr>", desc = "Fuzzy Fuzzer files" },
-            { "<space>lr", "<cmd>FzfLua lsp_references<cr>", desc = "Fuzzy Fuzzer references" },
-        },
+        'nvim-treesitter/nvim-treesitter',
+        cmd = 'TSUpdate',
+        config = function()
+            require'nvim-treesitter.configs'.setup {
+                ensure_installed = { "rust", "c", "lua", "vim", "help", "query" },
+
+                sync_install = false,
+                auto_install = true,
+
+                highlight = {
+                    enable = true,
+                    additional_vim_regex_highlighting = false,
+                },
+            }
+        end,
     },
     {
         "folke/which-key.nvim",
+        lazy = false,
         config = function()
             vim.o.timeout = true
             vim.o.timeoutlen = 300
-            require("which-key").setup({
+            require("which-key").register({
             })
         end,
     },
     {
         'nvim-lualine/lualine.nvim',
         lazy = false,
+        keys = {
+            { "<space>w", "<cmd>w<cr>", desc = "Save" },
+            { "<space>q", "<cmd>q<cr>", desc = "Quit" },
+            { "<space>x", "<cmd>x<cr>", desc = "Save and Quit" },
+        },
         config = function()
             require("lualine").setup({
                 options = {
@@ -48,13 +86,6 @@ return {
                 }
             })
         end,
-    },
-    {
-        "kdheepak/lazygit.nvim",
-        cmd = "LazyGit",
-        keys = {
-            { "<space>g", "<cmd>LazyGit<cr>", desc = "Launch Lazy Git" },
-        },
     },
     {
         "nvim-tree/nvim-tree.lua",
@@ -65,6 +96,7 @@ return {
             require("nvim-tree").setup {}
         end,
         keys = {
+            { "<space>n", desc = "Tree selector" },
             { "<space>nt", "<cmd>NvimTreeToggle<cr>", desc = "Toggle the Tree selector" },
             { "<space>ng", "<cmd>NvimTreeFocus<cr>", desc = "Select the Tree selector" },
             { "<space>nc", "<cmd>NvimTreeCollapse<cr>", desc = "Collapse the Tree selector" },
@@ -76,11 +108,32 @@ return {
         dependencies = { 'nvim-lua/plenary.nvim' },
         keys = {
             { "<space>f", "<cmd>Telescope find_files<cr>", desc = "Find file with Telescope" },
+            { "<space>r", "<cmd>Telescope lsp_references<cr>", desc = "Find references with Telescope" },
         },
     },
     {
         'romgrk/barbar.nvim',
+        lazy = false,
         dependencies = 'nvim-tree/nvim-web-devicons',
+        keys = {
+            { "<space>t", desc = "Tab" },
+            { "<space>t+", "<cmd>tabnew<cr>", desc = "New tab" },
+            { '<space>tp', '<Cmd>BufferPrevious<CR>', desc= "Go to Previous Tab" };
+            { '<space>tn', '<Cmd>BufferNext<CR>', desc= "Go to Next Tab" };
+            { '<space>tP', '<Cmd>BufferMovePrevious<CR>', desc= "Move to Previous Tab" };
+            { '<space>tN', '<Cmd>BufferMoveNext<CR>', desc= "Move to Next Tab" };
+            { '<space>t1', '<Cmd>BufferGoto 1<CR>', desc= "Go to Tab 1" };
+            { '<space>t2', '<Cmd>BufferGoto 2<CR>', desc= "Go to Tab 2" };
+            { '<space>t3', '<Cmd>BufferGoto 3<CR>', desc= "Go to Tab 3" };
+            { '<space>t4', '<Cmd>BufferGoto 4<CR>', desc= "Go to Tab 4" };
+            { '<space>t5', '<Cmd>BufferGoto 5<CR>', desc= "Go to Tab 5" };
+            { '<space>t6', '<Cmd>BufferGoto 6<CR>', desc= "Go to Tab 6" };
+            { '<space>t7', '<Cmd>BufferGoto 7<CR>', desc= "Go to Tab 7" };
+            { '<space>t8', '<Cmd>BufferGoto 8<CR>', desc= "Go to Tab 8" };
+            { '<space>t9', '<Cmd>BufferGoto 9<CR>', desc= "Go to Tab 9" };
+            { '<space>tl', '<Cmd>BufferLast<CR>', desc= "Go to Last Tab" };
+            { '<space>tc', '<Cmd>BufferClose<CR>', desc= "Close Tab" };
+        },
     },
     {
         'glepnir/dashboard-nvim',
@@ -114,5 +167,59 @@ return {
             }
         end,
         dependencies = { {'nvim-tree/nvim-web-devicons'}}
+    },
+    {
+        'nvim-lua/plenary.nvim',
+    },
+    {
+        'ThePrimeagen/harpoon',
+        keys = {
+            { "<space>h", desc = "Harpoon" },
+            { "<space>h+", '<cmd>lua require("harpoon.mark").add_file()<cr>', desc = "Add file to Harpoon" },
+            { "<space>ha", '<cmd>lua require("harpoon.mark").add_file()<cr>', desc = "Add file to Harpoon" },
+            { "<space>hl", '<cmd>lua require("harpoon.ui").toggle_quick_menu()<cr>', desc = "List files to Harpoon" },
+            { "<space>hn", '<cmd>lua require("harpoon.ui").nav_next()<cr>', desc = "Next file to Harpoon" },
+            { "<space>hp", '<cmd>lua require("harpoon.ui").nav_prev()<cr>', desc = "Prev file to Harpoon" },
+            { "<space>ht", '<cmd>lua require("harpoon.term").gotoTerminal(1)<cr>', desc = "Prev file to Harpoon" },
+        },
+    },
+    {
+        'akinsho/toggleterm.nvim',
+        version = "*",
+        config = true,
+        keys = {
+            { "<space><enter>", '<cmd>:ToggleTerm size=40 direction=float<cr>', desc = "Spawn floating Terminal" },
+            { "<space>Tv", '<cmd>:ToggleTerm size=40 direction=vertical<cr>', desc = "Spawn floating Terminal" },
+            { "<space>Th", '<cmd>:ToggleTerm size=20 direction=horizontal<cr>', desc = "Spawn floating Terminal" },
+        },
+        config = function()
+            require("toggleterm").setup {
+                persist_size = false,
+                float_opts = { -- 'single' | 'double' | 'shadow' | 'curved'
+                    border = "curved",
+                },
+            }
+        end,
+    },
+    {
+        'folke/todo-comments.nvim',
+        lazy = true,
+        dependencies = { 'nvim-lua/plenary.nvim' },
+        keys = {
+            { "<space>f", "<cmd>Telescope find_files<cr>", desc = "Find file with Telescope" },
+            -- { "<space>r", "<cmd>Telescope lsp_references<cr>", desc = "Find references with Telescope" },
+        },
+        config = function()
+            require("todo-comments").setup {
+                -- your configuration comes here
+                -- or leave it empty to use the default settings
+                -- refer to the configuration section below
+            }
+        end,
+        keys = {
+            { "<space>s", desc = "TODO Search" },
+            { "<space>sq", "<cmd>TodoQuickFix<cr>", desc = "TODO Quick Fix" },
+            { "<space>sf", "<cmd>TodoTelescope keywords=TODO,FIX<cr>", desc = "TODO Quick Fix" },
+        },
     },
 }
