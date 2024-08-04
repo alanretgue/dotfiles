d3a77cbc3efd9a0d1fee033e52897520beb92b84
Alan GUERET <alan.gueret@epita.fr>
2024-08-04 15:43:02 +0200

    Add many things and fix things

--- config/nvim/lua/plugins/init.lua
+++ config/nvim/lua/plugins/init.lua
@@ -27,35 +27,85 @@ return {
             {'hrsh7th/cmp-nvim-lua'},     -- Optional
 
             -- Snippets
-            {'L3MON4D3/LuaSnip'},             -- Required
+            {
+              "L3MON4D3/LuaSnip",
+              -- follow latest release.
+              version = "v2.*", -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+              -- install jsregexp (optional!).
+              build = "make install_jsregexp"
+            },
             {'rafamadriz/friendly-snippets'}, -- Optional
         },
         config = function()
-            local lsp = require('lsp-zero').preset({
-                name = 'minimal',
-                set_lsp_keymaps = true,
-                manage_nvim_cmp = true,
-                suggest_lsp_servers = false,
-            })
+          local lsp = require('lsp-zero').preset({
+            name = 'minimal',
+            set_lsp_keymaps = true,
+            manage_nvim_cmp = true,
+            suggest_lsp_servers = false,
+          })
 
-            -- (Optional) Configure lua language server for neovim
-            lsp.nvim_workspace()
-            -- vim.diagnostic.show()
+          -- (Optional) Configure lua language server for neovim
+          lsp.nvim_workspace()
+          -- vim.diagnostic.show()
 
-            lsp.setup()
+          lsp.setup()
         end,
-    },
-    {
-        "https://git.sr.ht/~whynothugo/lsp_lines.nvim",
+      },
+      {
+        "dgagn/diagflow.nvim",
+        lazy=false,
         config = function()
-            require("lsp_lines").setup()
-            vim.diagnostic.config({
-                virtual_text = false,
-            })
+          local opts = {
+            placement = 'inline',
+            scope = 'cursor',
+            enable = true,
+            show_borders = false,
+            show_sign = false,
+            inline_padding_left = 3,
+          }
+          require('diagflow').setup(opts)
         end,
         keys = {
-            { "<space>Ds", "<cmd>lua require('lsp_lines').toggle()<cr>" , desc = "Toggle Horizontal Console Diagnostic" },
+            { "<space>Ds", "<cmd>lua require('diagflow').toggle()<cr>" , desc = "Toggle Inline Diagnostics" },
+            { "<space>Df", "<cmd>lua vim.lsp.buf.code_action()<cr>" , desc = "Show QuickFix" },
         },
+    },
+    {
+      "gelguy/wilder.nvim",
+      lazy=false,
+      config = function()
+        local wilder = require('wilder')
+        wilder.setup({modes = {':', '/', '?'}})
+        local gradient = {
+          '#f4468f', '#fd4a85', '#ff507a', '#ff566f', '#ff5e63',
+          '#ff6658', '#ff704e', '#ff7a45', '#ff843d', '#ff9036',
+          '#f89b31', '#efa72f', '#e6b32e', '#dcbe30', '#d2c934',
+          '#c8d43a', '#bfde43', '#b6e84e', '#aff05b'
+        }
+
+        for i, fg in ipairs(gradient) do
+          gradient[i] = wilder.make_hl('WilderGradient' .. i, 'Pmenu', {{a = 1}, {a = 1}, {foreground = fg}})
+        end
+
+        wilder.set_option('renderer', wilder.popupmenu_renderer(
+        wilder.popupmenu_palette_theme({
+          -- 'single', 'double', 'rounded' or 'solid'
+          -- can also be a list of 8 characters, see :h wilder#popupmenu_palette_theme() for more details
+          border = 'rounded',
+          max_height = '75%',      -- max height of the palette
+          min_height = 0,          -- set to the same as 'max_height' for a fixed height window
+          prompt_position = 'top', -- 'top' or 'bottom' to set the location of the prompt
+          reverse = 0,             -- set to 1 to reverse the order of the list, use in combination with 'prompt_position'
+          highlights = {
+            gradient = gradient, -- must be set
+            -- selected_gradient key can be set to apply gradient highlighting for the selected candidate.
+          },
+          highlighter = wilder.highlighter_with_gradient({
+            wilder.lua_fzy_highlighter(),
+          }),
+        })
+        ))
+      end,
     },
     {
         "folke/trouble.nvim",
@@ -69,7 +119,7 @@ return {
             }
         end,
         keys = {
-            { "<space>Dl", "<cmd>TroubleToggle document_diagnostics<cr>" , desc = "Toggle Infile Diagnostic" },
+            { "<space>Dl", "<cmd>Trouble diagnostics toggle filter.buf=0<cr>" , desc = "Toggle Infile Diagnostic" },
         },
     },
     {
@@ -105,6 +155,9 @@ return {
     {
         "folke/which-key.nvim",
         lazy = false,
+        dependencies = {
+          {'echasnovski/mini.nvim'}
+        },
         config = function()
             vim.o.timeout = true
             vim.o.timeoutlen = 300
@@ -161,14 +214,17 @@ return {
     {
         'nvim-telescope/telescope.nvim',
         lazy = false,
-        dependencies = { 'nvim-lua/plenary.nvim' },
+        dependencies = {
+          {'nvim-lua/plenary.nvim' },
+          {'nvim-telescope/telescope-ui-select.nvim' },
+        },
         keys = {
             { "<space>f", "<cmd>Telescope find_files<cr>", desc = "Find file with Telescope" },
             { "<space>F", "<cmd>Telescope live_grep<cr>", desc = "Grep with Telescope" },
             { "<space>d", "<cmd>Telescope lsp_definitions<cr>", desc = "Find Definition with Telescope" },
             { "<space>i", "<cmd>Telescope lsp_incoming_calls<cr>", desc = "Find Implementations with Telescope" },
             { "<space>o", "<cmd>Telescope oldfiles<cr>", desc = "Find recently open files with Telescope" },
-            { "<C-f>", "<cmd>Telescope grep_string<cr>", desc = "Find recently open files with Telescope" },
+            { "<C-f>", "<cmd>Telescope grep_string<cr>", desc = "Find this symbole" },
             { "<space><tab>", "<cmd>Telescope buffers<cr>", desc = "Opened buffers with Telescope" },
             { "<space>c", "<cmd>Telescope resume<cr>", desc = "Resume the preview search with Telescope" },
             { "<space>gC", "<cmd>Telescope git_commits<cr>", desc = "List Commits with Telescope" },
@@ -177,6 +233,14 @@ return {
             { "<space>gs", "<cmd>Telescope git_status<cr>", desc = "Git Status with Telescope" },
         },
         config = function()
+          extensions = {
+            ["ui-select"] = {
+              require("telescope.themes").get_dropdown {
+                -- even more opts
+              }
+            }
+          }
+          require("telescope").load_extension("ui-select")
         end,
     },
     {
@@ -292,6 +356,7 @@ return {
             { "<space>gb", "<cmd>Gitsigns toggle_current_line_blame<cr>", desc = "git diff of the block" },
             { "<space>gn", "<cmd>Gitsigns next_hunk<cr>", desc = "Go to the next not staged hunk" },
             { "<space>gp", "<cmd>Gitsigns prev_hunk<cr>", desc = "Go to the previous not staged hunk" },
+            { "<space>gr", "<cmd>Gitsigns reset_hunk<cr>", desc = "Reset the current hunk" },
         },
         config = function()
             require('gitsigns').setup({
